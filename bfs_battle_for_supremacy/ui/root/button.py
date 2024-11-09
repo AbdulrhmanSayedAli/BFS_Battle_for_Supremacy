02b7bde99d9830d8d14659c089ff73ca4c5b18e7
abdulrhman.ali@bld.ai <abdulrhman.ali@bld.ai>
2024-11-09 16:50:51 +0300

    create button element

---
+++ bfs_battle_for_supremacy/ui/root/button.py
@@ -0,0 +1,53 @@
+from pygame.event import Event
+from ui.root.component import Component
+from typing import Callable
+import pygame
+
+
+class Button(Component):
+    def __init__(
+        self,
+        x: float,
+        y: float,
+        width: float,
+        height: float,
+        color,
+        secondary_color,
+        text: str,
+        font: pygame.font.Font,
+        on_click: Callable = None,
+        on_hover: Callable = None,
+        on_hover_end: Callable = None,
+    ):
+        super().__init__(
+            x,
+            y,
+            width,
+            height,
+            on_click,
+            on_hover,
+            on_hover_end,
+        )
+        self.color = color
+        self.secondary_color = secondary_color
+        self.text = text
+        self.font = font
+
+    def draw(self, screen):
+        self.fill(self.color)
+
+        text_surf = self.font.render(self.text, True, (255, 255, 255))
+        text_rect = text_surf.get_rect(
+            center=(self.rect.width // 2, self.rect.height // 2)
+        )
+        self.blit(text_surf, text_rect)
+
+        super().draw(screen)
+
+    def _on_hover_listener(self, event: Event):
+        self.color, self.secondary_color = self.secondary_color, self.color
+        super()._on_hover_listener(event)
+
+    def _on_hover_end_listener(self, event: Event):
+        self.color, self.secondary_color = self.secondary_color, self.color
+        super()._on_hover_listener(event)
