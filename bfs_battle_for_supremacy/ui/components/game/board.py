05b77c5f58a26bef3f57014eb5409b42a4198158
abdulrhman.ali@bld.ai <abdulrhman.ali@bld.ai>
2024-11-14 14:31:47 +0300

    move setup tiles logic into board component

--- bfs_battle_for_supremacy/ui/components/game/board.py
+++ bfs_battle_for_supremacy/ui/components/game/board.py
@@ -1,6 +1,7 @@
 from typing import Callable
 from ui.root.container import Container
 from ui.components.game.tile import Tile
+from config import BOARD_SIZE_WIDTH, BOARD_SIZE_HEIGHT, IMAGES_PATH
 
 
 class Board(Container):
@@ -10,20 +11,56 @@ class Board(Container):
         y: float,
         width: float,
         height: float,
-        tiles: list[list[Tile]],
         on_click: Callable = None,
         on_hover: Callable = None,
         on_hover_end: Callable = None,
     ):
-        components = [tile for row in tiles for tile in row]
+        self._tiles = self.setup_tiles(width, height)
+        components = [tile for row in self.tiles for tile in row]
         super().__init__(
             x, y, width, height, components, on_click, on_hover, on_hover_end
         )
 
+    @property
+    def tiles(self) -> list[list[Tile]]:
+        return self._tiles
+
+    @tiles.setter
+    def tiles(self, tiles):
+        self._tiles = tiles
+        self.components = [tile for row in tiles for tile in row]
+
+    def setup_tiles(self, width, height) -> list[list[Tile]]:
+        tile_height = height / BOARD_SIZE_HEIGHT
+        tile_width = width / BOARD_SIZE_WIDTH
+        res: list[list[Tile]] = []
+        for i in range(BOARD_SIZE_HEIGHT):
+            cur_res: list[Tile] = []
+            for j in range(BOARD_SIZE_WIDTH):
+                cur_res.append(
+                    Tile(
+                        j * tile_width,
+                        i * tile_height,
+                        tile_width,
+                        tile_height,
+                        j,
+                        i,
+                        on_click=lambda c, e: self.select_tile(c.pos),
+                    )
+                )
+            res.append(cur_res)
+        res[0][0].image_path = IMAGES_PATH + "player1.webp"
+        res[BOARD_SIZE_HEIGHT - 1][BOARD_SIZE_WIDTH - 1].image_path = (
+            IMAGES_PATH + "player2.webp"
+        )
+        return res
+
     def select_tile(self, pos):
-        if self.components[pos].selected:
-            self.components[pos].selected = False
+        tile: Tile = self.components[pos]
+        if tile.selected:
+            self.tiles[tile.pos_y][tile.pos_x].selected = False
             return
-        for i in range(len(self.components)):
-            self.components[i].selected = False
-        self.components[pos].selected = True
+        for i in range(len(self.tiles)):
+            for j in range(len(self.tiles[i])):
+                self.tiles[i][j].selected = False
+        self.tiles[tile.pos_y][tile.pos_x].selected = True
--- bfs_battle_for_supremacy/ui/layouts/game.py
+++ bfs_battle_for_supremacy/ui/layouts/game.py
@@ -1,9 +1,8 @@
 from ui.root.layout import Layout
 from ui.root.button import Button
 from ui.components.game.board import Board
-from ui.components.game.tile import Tile
 from ui.utils import get_font
-from config import BOARD_SIZE_WIDTH, BOARD_SIZE_HEIGHT, IMAGES_PATH, WIDTH
+from config import WIDTH
 from ui.utils import main_color, main_color_hover
 
 BOARD_WIDTH = 700
@@ -15,37 +14,8 @@ STATS_HEIGHT = 200
 
 
 class Game(Layout):
-    def setup_tiles(self, width, height) -> list[list[Tile]]:
-        tile_height = height / BOARD_SIZE_HEIGHT
-        tile_width = width / BOARD_SIZE_WIDTH
-        res: list[list[Tile]] = []
-        for i in range(BOARD_SIZE_HEIGHT):
-            cur_res: list[Tile] = []
-            for j in range(BOARD_SIZE_WIDTH):
-                cur_res.append(
-                    Tile(
-                        j * tile_width,
-                        i * tile_height,
-                        tile_width,
-                        tile_height,
-                        j,
-                        i,
-                        on_click=lambda c, e: self.board.select_tile(c.pos),
-                    )
-                )
-            res.append(cur_res)
-        res[0][0].image_path = IMAGES_PATH + "player1.webp"
-        res[BOARD_SIZE_HEIGHT - 1][BOARD_SIZE_WIDTH - 1].image_path = (
-            IMAGES_PATH + "player2.webp"
-        )
-        return res
-
     def setup_components(self):
-
-        self.tiles: list[list[Tile]] = self.setup_tiles(
-            BOARD_WIDTH, BOARD_HEIGHT
-        )
-        self.board: Board = Board(0, 0, BOARD_WIDTH, BOARD_HEIGHT, self.tiles)
+        self.board: Board = Board(0, 0, BOARD_WIDTH, BOARD_HEIGHT)
 
         self.selection = Button(
             BOARD_WIDTH,
